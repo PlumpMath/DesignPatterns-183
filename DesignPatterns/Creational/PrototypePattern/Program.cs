337c516973ea01106ca09e8cf7ea90c4ccae8982
qcjwq <qcjwq@vip.qq.com>
2015-10-25 21:01:56 +0800

    原型模式

---
+++ DesignPatterns/Creational/PrototypePattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcretePrototype1 p1 = new ConcretePrototype1("I");
+            ConcretePrototype1 p2 = (ConcretePrototype1)p1.Clone();
+
+            Console.WriteLine($"befor clone, Id={p1.Id}");
+            Console.WriteLine($"after clone, Id={p2.Id}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatterns/Creational/PrototypePattern/Prototype.cs
@@ -0,0 +1,14 @@
+﻿namespace PrototypePattern
+{
+    public abstract class Prototype
+    {
+        public string Id { get; set; }
+
+        public Prototype(string id)
+        {
+            Id = id;
+        }
+
+        public abstract Prototype Clone();
+    }
+}
