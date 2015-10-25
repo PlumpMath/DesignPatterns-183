acc4b4417d83257550f35c95987e955653c7ae1e
qcjwq <qcjwq@vip.qq.com>
2015-10-25 21:20:04 +0800

    模板方法

---
+++ DesignPatterns/Behavioral/TemplateMethodPattern/ConcreteClassB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TemplateMethodPattern
+{
+    public class ConcreteClassB:AbstractClass
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("具体累B方法1实现");
+        }
+
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("具体类B方法2实现");
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/TemplateMethodPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass c = new ConcreteClassA();
+            c.TemplateMethod();
+           
+            c = new ConcreteClassB();
+            c.TemplateMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
