caac5c341123abbf3341d5b8b3529a9c49ac18c4
qcjwq <qcjwq@vip.qq.com>
2015-11-01 17:56:06 +0800

    状态模式

---
+++ DesignPatterns/Behavioral/StatePattern/Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StatePattern
+{
+    public class Context
+    {
+        private State _state;
+
+        public State State
+        {
+            get { return _state; } 
+            set
+            {
+                _state = value;
+                Console.WriteLine($"当前状态：{_state.GetType().Name}");
+            }
+        }
+
+        public Context(State state)
+        {
+            State = state;
+        }
+
+        public void Request()
+        {
+            State.Handle(this);
+        }
+    }
+}
