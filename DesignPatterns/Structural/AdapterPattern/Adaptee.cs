e12231985aa6e33e70e06d24a6f13b3c5aa104dd
qcjwq <qcjwq@vip.qq.com>
2015-11-14 18:15:09 +0800

    适配器模式

---
+++ DesignPatterns/Structural/AdapterPattern/Adaptee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    public class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("特殊请求！");
+        }
+    }
+}
---
+++ DesignPatterns/Structural/AdapterPattern/Target.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    public class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("普通请求！");
+        }
+    }
+}
